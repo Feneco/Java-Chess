d7cabb1944f4381235b8470bd93a94f8d3dc27f1
Wagner <wagnerweb2010@gmail.com>
2023-03-27 01:05:13 -0300

    Added start message

--- src/main/java/xyz/feneco/Main.java
+++ src/main/java/xyz/feneco/Main.java
@@ -4,6 +4,7 @@ import xyz.feneco.frontend.ChessGame;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("New game!");
         ChessGame ch = new ChessGame();
         ch.init();
     }
