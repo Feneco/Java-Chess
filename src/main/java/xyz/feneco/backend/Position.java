3973d3be38a104830fadb19ccebf1215dda5b099
Wagner <wagnerweb2010@gmail.com>
2023-03-31 15:07:22 -0300

    removed unused methods and changed some signatures

--- src/main/java/xyz/feneco/backend/Position.java
+++ src/main/java/xyz/feneco/backend/Position.java
@@ -1,8 +1,8 @@
 package xyz.feneco.backend;
 
 public class Position {
-    private final Integer x;
-    private final Integer y;
+    private final int x;
+    private final int y;
 
     public Position(Integer x, Integer y) {
         this.x = x;
@@ -14,54 +14,40 @@ public class Position {
         this.y = pos.getY();
     }
 
-    public Integer getX() {
+    public int getX() {
         return x;
     }
 
-    public Integer getY() {
+    public int getY() {
         return y;
     }
 
-    public static Position add(Position pos1, Position pos2) {
-        return new Position(pos1.getX() + pos2.getX(), pos1.getY() + pos2.getY());
-    }
-
-    public Position getAdd(Integer a, Integer b) {
+    public Position add(int a, int b) {
         return new Position(this.x + a, this.y + b);
     }
 
-    public Position getAdd(Position pos) {
+    public Position add(Position pos) {
         return new Position(this.x + pos.getX(), this.y + pos.getY());
     }
 
-    public static Position sub(Position pos1, Position pos2) {
-        return new Position(pos1.getX() - pos2.getX(), pos1.getY() - pos2.getY());
-    }
-
-    public Position getSub(Integer a, Integer b) {
+    public Position sub(int a, int b) {
         return new Position(this.x + a, this.y + b);
     }
 
-    public Position getSub(Position pos) {
+    public Position sub(Position pos) {
         return new Position(x - pos.getX(), y - pos.getY());
     }
 
-    public Boolean equals(Position pos) {
-        return (x.equals(pos.getX()) && y.equals(pos.getY()));
+    public boolean equals(Position pos) {
+        return x == pos.getX() && y == pos.getY();
     }
 
-    public static Boolean equals(Position pos1, Position pos2) {
-        return (pos1.equals(pos2));
-    }
-
-    public static Integer distanceL1(Position pos1, Position pos2) {
-        Integer a = Math.abs(pos1.getX() - pos2.getX());
-        Integer b = Math.abs(pos1.getY() - pos2.getY());
-        return a + b;
+    public static boolean equals(Position pos1, Position pos2) {
+        return pos1.equals(pos2);
     }
 
     @Override
     public String toString(){
-        return "[" + x.toString() + ", " + y.toString() + "]";
+        return "[" + x + ", " + y + "]";
     }
 }
