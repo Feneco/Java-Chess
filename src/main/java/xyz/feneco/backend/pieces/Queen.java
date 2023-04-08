dcfea43586d1d81f6adef0903956553056d8ff53
Wagner <wagnerweb2010@gmail.com>
2023-04-08 15:12:20 -0300

    create queen move mask

--- src/main/java/xyz/feneco/backend/pieces/Queen.java
+++ src/main/java/xyz/feneco/backend/pieces/Queen.java
@@ -1,6 +1,8 @@
 package xyz.feneco.backend.pieces;
 
 import xyz.feneco.backend.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Queen extends SlidingPieces {
@@ -10,7 +12,52 @@ public class Queen extends SlidingPieces {
 
     @Override
     protected List<Position> moveMask(Position position) {
-        // TODO
-        return null;
+        List<Position> ret = new ArrayList<>();
+        // There is probably a better way of doing this
+        for (int i = position.x(); i < 8; i++) {
+            for (int j = position.y(); j < 8; j++) {
+                if(i == position.y() || j == position.x()) {
+                    continue;
+                }
+                ret.add(new Position(i, j));
+            }
+        }
+        for (int i = position.x(); i >= 0; i--) {
+            for (int j = position.y(); j < 8; j++) {
+                if(i == position.y() || j == position.x()) {
+                    continue;
+                }
+                ret.add(new Position(i, j));
+            }
+        }
+        for (int i = position.x(); i >= 0; i--) {
+            for (int j = position.y(); j >= 0; j--) {
+                if(i == position.y() || j == position.x()) {
+                    continue;
+                }
+                ret.add(new Position(i, j));
+            }
+        }
+        for (int i = position.x(); i < 8; i++) {
+            for (int j = position.y(); j >= 0; j--) {
+                if(i == position.y() || j == position.x()) {
+                    continue;
+                }
+                ret.add(new Position(i, j));
+            }
+        }
+        for (int i = 0; i < 8; i++) {
+            if(i == position.y()){
+                continue;
+            }
+            ret.add(new Position(position.x(), i));
+        }
+        for (int j = 0; j < 8; j++) {
+            if(j == position.x()){
+                continue;
+            }
+            ret.add(new Position(j, position.y()));
+        }
+        return ret;
     }
 }
