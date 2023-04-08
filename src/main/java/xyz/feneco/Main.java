b7a72643a8e047fcc9b2066ad79787689acfbddd
Wagner <wagnerweb2010@gmail.com>
2023-04-08 14:28:28 -0300

    removed frontend from main

--- src/main/java/xyz/feneco/Main.java
+++ src/main/java/xyz/feneco/Main.java
@@ -1,11 +1,7 @@
 package xyz.feneco;
 
-import xyz.feneco.frontend.ChessGame;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("New game!");
-        ChessGame ch = new ChessGame();
-        ch.init();
     }
 }
