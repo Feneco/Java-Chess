155798975f946097411e15cbc8af7cc4affa39e7
Wagner <wagnerweb2010@gmail.com>
2023-04-08 16:45:40 -0300

    removed bugs from sliding pieces

--- src/main/java/xyz/feneco/backend/SlidingPieces.java
+++ src/main/java/xyz/feneco/backend/SlidingPieces.java
@@ -13,12 +13,7 @@ public abstract class SlidingPieces extends Piece {
 
     @Override
     public boolean canMove(Position desiredPosition, Board board) {
-        List<Position> mask = moveMask(desiredPosition);
-        mask.sort((p1, p2) -> {
-            int sp1 = 8 * p1.y() + p1.x();
-            int sp2 = 8 * p2.y() + p2.x();
-            return Integer.compare(sp1, sp2);
-        });
+        List<Position> mask = moveMask(position);
         for (Position slot: mask) {
             if ( slot.equals(desiredPosition) ) {
                 // Check if piece at the desired position is enemy, if friend return false
@@ -31,20 +26,24 @@ public abstract class SlidingPieces extends Piece {
                 // Defining start values
                 Position delta = desiredPosition.sub(position);
                 int dx = Integer.signum(delta.x());
-                int xStartPos = desiredPosition.x() - dx;
+                int xStartPos = position.x();
+                int xEndPos = desiredPosition.x();
                 int dy = Integer.signum(delta.y());
-                int yStartPos = desiredPosition.y() - dy;
+                int yStartPos = position.y();
+                int yEndPos = desiredPosition.y();
+
+                int maxCount = Math.max(Math.abs(delta.x()), Math.abs(delta.y()));
+                Position testpos = new Position(xStartPos + dx, yStartPos + dy);
+                for(int count = 1; count < maxCount; count++){
+                    if(board.isPieceAt(testpos)){
+                        return false;
+                    }
+                    testpos = testpos.add(dx, dy);
+                }
                 // check for a piece at every square between the desired position towards this piece position.
                 // In case there is, return false. This won't check this piece's position and the desiredPosition,
                 // only spaces between them, marching "backwards".
-                for (int i = xStartPos; i != position.x(); i -= dx) {
-                    for (int j = yStartPos; j != position.y(); j -= dy) {
-                        Position testPos = new Position(i, j);
-                        if ( board.isPieceAt(testPos) ){
-                            return false;
-                        }
-                    }
-                }
+
                 return true;
             }
         }
