f4dafd86ba5a86848e5cd09fc20092e596c7189f
Wagner <wagnerweb2010@gmail.com>
2023-04-08 15:12:08 -0300

    created bishop move mask

--- src/main/java/xyz/feneco/backend/pieces/Bishop.java
+++ src/main/java/xyz/feneco/backend/pieces/Bishop.java
@@ -1,6 +1,8 @@
 package xyz.feneco.backend.pieces;
 
 import xyz.feneco.backend.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bishop extends SlidingPieces {
@@ -11,7 +13,40 @@ public class Bishop extends SlidingPieces {
 
     @Override
     protected List<Position> moveMask(Position position) {
-        // TODO
-        return null;
+        List<Position> ret = new ArrayList<>();
+        // There is probably a better way of doing this
+        for (int i = position.x(); i < 8; i++) {
+            for (int j = position.y(); j < 8; j++) {
+                if(i == position.y() || j == position.x()) {
+                    continue;
+                }
+                ret.add(new Position(i, j));
+            }
+        }
+        for (int i = position.x(); i >= 0; i--) {
+            for (int j = position.y(); j < 8; j++) {
+                if(i == position.y() || j == position.x()) {
+                    continue;
+                }
+                ret.add(new Position(i, j));
+            }
+        }
+        for (int i = position.x(); i >= 0; i--) {
+            for (int j = position.y(); j >= 0; j--) {
+                if(i == position.y() || j == position.x()) {
+                    continue;
+                }
+                ret.add(new Position(i, j));
+            }
+        }
+        for (int i = position.x(); i < 8; i++) {
+            for (int j = position.y(); j >= 0; j--) {
+                if(i == position.y() || j == position.x()) {
+                    continue;
+                }
+                ret.add(new Position(i, j));
+            }
+        }
+        return ret;
     }
 }
