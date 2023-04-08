07103d49d04542a79f998f69277439da24084e5a
Wagner <wagnerweb2010@gmail.com>
2023-04-08 15:59:21 -0300

    fix sub signal error

--- src/main/java/xyz/feneco/backend/Position.java
+++ src/main/java/xyz/feneco/backend/Position.java
@@ -1,17 +1,8 @@
 package xyz.feneco.backend;
 
-public class Position {
-    private final int x;
-    private final int y;
-
-    public Position(Integer x, Integer y) {
-        this.x = x;
-        this.y = y;
-    }
-
+public record Position ( int x, int y ) {
     public Position(Position pos) {
-        this.x = pos.x();
-        this.y = pos.y();
+        this(pos.x(), pos.y());
     }
 
     public int x() { return x; }
@@ -20,7 +11,7 @@ public class Position {
     public Position add(int a, int b) { return new Position(this.x + a, this.y + b); }
     public Position add(Position pos) { return this.add(pos.x(), pos.y()); }
 
-    public Position sub(int a, int b) { return new Position(this.x + a, this.y + b); }
+    public Position sub(int a, int b) { return new Position(this.x - a, this.y - b); }
     public Position sub(Position pos) { return this.sub(pos.x(), pos.y()); }
 
     public boolean equals(Position pos) {
