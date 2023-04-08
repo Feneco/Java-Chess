780e72dd16cb2ed26293f1752e0d75a7d429dea7
Wagner <wagnerweb2010@gmail.com>
2023-04-08 15:44:34 -0300

    fixed board factory emptyBoard null bug

--- src/main/java/xyz/feneco/backend/BoardFactory.java
+++ src/main/java/xyz/feneco/backend/BoardFactory.java
@@ -1,11 +1,13 @@
 package xyz.feneco.backend;
 
+import java.util.ArrayList;
+
 public class BoardFactory {
     public static Board getStandardBoard() {
         return null;
     }
 
     public static Board getEmptyBoard() {
-        return new Board(null, null);
+        return new Board(new ArrayList<>(), new ArrayList<>());
     }
 }
