42a73c0240ff04bd0f677b6552892edff50c3357
Wagner <wagnerweb2010@gmail.com>
2023-04-16 21:25:29 -0300

    Wrote King canMove function and it's tests

--- src/main/java/xyz/feneco/backend/pieces/King.java
+++ src/main/java/xyz/feneco/backend/pieces/King.java
@@ -8,10 +8,40 @@ public class King extends Piece  {
         super(team, position, 'K', notMoved);
     }
 
-
     @Override
     public boolean canMove(Position desiredPosition, Board board) {
-        // TODO
-        return false;
+        Position delta = desiredPosition.sub(position);
+        boolean move1Distance = Math.max( Math.abs(delta.x()), Math.abs(delta.y()) ) == 1;
+        boolean startPosition = (position.y() == 0 || position.y() == 7) && (position.x() == 4);
+        // Might be castling
+        // Check if it is in the starting chess position (as we might start the match halfway through)
+        if (!move1Distance && notMoved() && startPosition && desiredPosition.y() == position.y()) {
+            // kingSide will be 1 if it is kingSide, -1 if queenside, 0 if none.
+            int kingSide = desiredPosition.x() == 6 ? 1 : (desiredPosition.x() == 1 ? -1 : 0);
+            Position rookTestPosition;
+            if (kingSide == 1) {
+                rookTestPosition = new Position(7, position.y());
+            } else if (kingSide == -1) {
+                rookTestPosition = new Position(0, position.y());
+            } else {
+                return false;
+            }
+            if (board.isPieceAt(rookTestPosition)) {
+                Piece mayBeRook = board.getPieceAt(rookTestPosition);
+                if (mayBeRook instanceof Rook rook) {
+                    if (kingSide == 1) {
+                        Position testPos1 = new Position(5, position.y());
+                        Position testPos2 = new Position(6, position.y());
+                        return !board.isPieceAt(testPos1) && !board.isPieceAt(testPos2) && rook.notMoved();
+                    } else if (kingSide == -1){
+                        Position testPos1 = new Position(1, position.y());
+                        Position testPos2 = new Position(2, position.y());
+                        Position testPos3 = new Position(3, position.y());
+                        return !board.isPieceAt(testPos1) && !board.isPieceAt(testPos2) && !board.isPieceAt(testPos3) && rook.notMoved();
+                    }
+                }
+            }
+        }
+        return move1Distance;
     }
 }
---
+++ src/test/java/xyz/feneco/backend/pieces/KingTest.java
@@ -0,0 +1,53 @@
+package xyz.feneco.backend.pieces;
+
+import org.junit.jupiter.api.Test;
+import xyz.feneco.backend.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KingTest {
+
+    @Test
+    void canMove() {
+        {
+            Piece wKing1 = new King(Team.White, new Position(4, 0), true);
+            Piece wRook1 = new Rook(Team.White, new Position(7, 0), true);
+            Piece wRook2 = new Rook(Team.White, new Position(0, 0), true);
+
+            Piece bQueen1 = new Queen(Team.Black, new Position(3, 1), true);
+
+            Board b = BoardFactory.getEmptyBoard();
+            b.addPiece(wKing1);
+            b.addPiece(wRook1);
+            b.addPiece(wRook2);
+            b.addPiece(bQueen1);
+
+            assertTrue(wKing1.canMove(new Position(4, 1), b));
+            assertTrue(wKing1.canMove(new Position(6, 0), b));
+            assertTrue(wKing1.canMove(new Position(1, 0), b));
+            assertTrue(wKing1.canMove(bQueen1.getPosition(), b));
+
+            assertFalse(wKing1.canMove(new Position(6, 1), b));
+        }
+        {
+            Piece wKing1 = new King(Team.White, new Position(4, 0), true);
+            Piece wRook1 = new Rook(Team.White, new Position(7, 0), false);
+            Piece wRook2 = new Rook(Team.White, new Position(0, 0), false);
+
+            Piece bQueen1 = new Queen(Team.Black, new Position(3, 1), true);
+
+            Board b = BoardFactory.getEmptyBoard();
+            b.addPiece(wKing1);
+            b.addPiece(wRook1);
+            b.addPiece(wRook2);
+            b.addPiece(bQueen1);
+
+            assertTrue(wKing1.canMove(new Position(4, 1), b));
+            assertFalse(wKing1.canMove(new Position(6, 0), b));
+            assertFalse(wKing1.canMove(new Position(1, 0), b));
+            assertTrue(wKing1.canMove(bQueen1.getPosition(), b));
+
+            assertFalse(wKing1.canMove(new Position(6, 1), b));
+        }
+    }
+}
