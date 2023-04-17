133a41d5d7f865f3a6ca8fa3806b5583b1bd894a
Wagner <wagnerweb2010@gmail.com>
2023-04-16 22:57:27 -0300

    Created pawns and Pawns tests

--- src/main/java/xyz/feneco/backend/pieces/Pawn.java
+++ src/main/java/xyz/feneco/backend/pieces/Pawn.java
@@ -14,7 +14,50 @@ public class Pawn extends Piece {
 
     @Override
     public boolean canMove(Position desiredPosition, Board board) {
-        // TODO
+        Position delta = desiredPosition.sub(position);
+        boolean forwardDiagonalMovement = Math.abs(delta.x()) == 1 && delta.y() == getTeam().getDirection() ;
+        boolean move1AheadMovement = delta.y() == 1 * getTeam().getDirection() && delta.x() == 0;
+        boolean move2AheadMovement = delta.y() == 2 * getTeam().getDirection() && delta.x() == 0 && notMoved();
+        if (forwardDiagonalMovement) {
+            Position enPassantTestPosition = desiredPosition.sub(0, getTeam().getDirection());
+            if(board.isPieceAt(desiredPosition)) {
+                Piece testPiece = board.getPieceAt(desiredPosition);
+                return testPiece.isEnemy(this);
+            } else if ( board.isPieceAt(enPassantTestPosition) && !board.isPieceAt(desiredPosition) ) { // En-Passant check
+                Piece testPiece = board.getPieceAt(enPassantTestPosition);
+                if (testPiece instanceof Pawn pawn) {
+                    return this.isEnemy(pawn) && pawn.getEnPassant();
+                }
+            }
+        } else if (move2AheadMovement) {
+            boolean testPos1Clear = !board.isPieceAt(position.add(0, 1 * getTeam().getDirection()));
+            boolean testPos2Clear = !board.isPieceAt(position.add(0, 2 * getTeam().getDirection()));
+            return testPos1Clear
+                    && testPos2Clear;
+        } else if (move1AheadMovement) {
+            boolean testPos1Clear = !board.isPieceAt(position.add(0, 1 * getTeam().getDirection()));
+            return testPos1Clear;
+        }
         return false;
+    }
+
+    public boolean getEnPassant(){
+        return enPassant;
+    }
+
+    public void setEnPassant(boolean var){
+        enPassant = var;
+    }
+
+    @Override
+    public void move(Position toPosition) {
+        enPassant = false;
+        if(notMoved()) {
+            Position delta = toPosition.sub(position);
+            if(delta.x()==0 && delta.y() == 2 * getTeam().getDirection()) {// Check if movement is two forward
+                setEnPassant(true);
+            }
+        }
+        super.move(toPosition);
     }
 }
---
+++ src/test/java/xyz/feneco/backend/pieces/PawnTest.java
@@ -0,0 +1,66 @@
+package xyz.feneco.backend.pieces;
+
+import org.junit.jupiter.api.Test;
+import xyz.feneco.backend.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PawnTest {
+
+    @Test
+    void canMove() {
+        {
+            Board b = BoardFactory.getEmptyBoard();
+            Piece wPawn1 = new Pawn(Team.White, new Position(1, 1), true);
+            b.addPiece(wPawn1);
+
+            assertTrue(wPawn1.canMove(new Position(1, 3), b));
+            assertTrue(wPawn1.canMove(new Position(1, 2), b));
+            assertFalse(wPawn1.canMove(new Position(2, 2), b));
+            assertFalse(wPawn1.canMove(new Position(0, 2), b));
+
+            Piece wPawn2 = new Pawn(Team.White, new Position(0, 2), false);
+            b.addPiece(wPawn2);
+            Piece bPawn1 = new Pawn(Team.Black, new Position(2,2), false);
+            b.addPiece(bPawn1);
+            assertTrue(wPawn1.canMove(new Position(2, 2), b));
+            assertFalse(wPawn1.canMove(new Position(0, 2), b));
+        }
+        { // EnPassant test
+            Board b = BoardFactory.getEmptyBoard();
+            Piece wPawn1 = new Pawn(Team.White, new Position(1, 4), false);
+            b.addPiece(wPawn1);
+            Piece bPawn1 = new Pawn(Team.Black, new Position(2, 6), true);
+            b.addPiece(bPawn1);
+
+            assertFalse(wPawn1.canMove(new Position(1, 6), b));
+            assertFalse(wPawn1.canMove(new Position(2, 5), b));
+            assertTrue(wPawn1.canMove(new Position(1, 5), b));
+            bPawn1.move(new Position(2, 4));
+            assertTrue(wPawn1.canMove(new Position(2, 5), b));
+
+            Piece wPawn2 = new Pawn(Team.White, new Position(3, 4), false);
+            b.addPiece(wPawn2);
+            Piece bPawn2 = new Pawn(Team.Black, new Position(4, 4), true);
+            b.addPiece(bPawn2);
+            assertFalse(wPawn1.canMove(new Position(4, 5), b));
+        }
+    }
+
+    @Test
+    void move() {
+        Pawn wPawn1 = new Pawn(Team.White, new Position(1, 1), true);
+
+        assertFalse(wPawn1.getEnPassant());
+        wPawn1.move(new Position(1, 3));
+        assertTrue(wPawn1.getEnPassant());
+        wPawn1.move(new Position(1, 4));
+        assertFalse(wPawn1.getEnPassant());
+
+        Pawn wPawn2 = new Pawn(Team.White, new Position(1, 1), true);
+        wPawn1.move(new Position(1, 1));
+        assertFalse(wPawn1.getEnPassant());
+        wPawn1.move(new Position(1, 2));
+        assertFalse(wPawn1.getEnPassant());
+    }
+}
